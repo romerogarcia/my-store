b19c3db03c56c891575da570e5a45851b0fc89df
Noelia R <noeliaromga@gmail.com>
2022-08-23 13:13:47 +0200

    add lint module

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,11 @@
+///////Entonces las directivas se usan solo para modificar el DOM, pero no es recomendable hacer estas modificaciones al DOM de está manera según la documentación de Angular. Lo que quiere decir que no tiene mucha utilidad crear directivas.
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
+//escuchar el evento + crearlo
   @HostListener('mouseenter') onMouseEnter = () => {
     this.element.nativeElement.style.backgroundColor = 'rgb(193,226,198)';
   }
